04aa5d830e72f9743920db54f148359e57b655c8
IsraelAzoulay <israelazoulay1@gmail.com>
2022-12-05 17:38:34 +0200

    Connecting tha backend to the frontend. Modifying a bit of the backend by moving some of it to the front and finishing up the app.

--- backend.py
+++ backend.py
@@ -6,7 +6,7 @@ import requests
 API_KEY = "41c97556715dcc5d59b4d5485d18875b"
 
 
-def get_data(place, forecast_days, kind):
+def get_data(place, forecast_days):
     # The url of the api of the 'openweathermap.org' website.
     url_api = f"http://api.openweathermap.org/data/2.5/forecast?" \
               f"q={place}&" \
@@ -18,22 +18,15 @@ def get_data(place, forecast_days, kind):
     # Filtering all the data we got in the 'json()' format, and getting all the 40 forecast updates which
     # are for the maximum days number - 5 days (since there is a forecast update every 3 hours).
     filtered_data = data["list"]
-    # Filtering now all the 40 hours forecast updates based on the 'forecast_days' the user entered.
-    # The minimum of forecast updates are 8 which are for the minimum days number - 1 day.
+    # Filtering now all the 40 hours forecast updates based on the 'forecast_days' the user entered,
+    # and returning it in a list to the main.py file. The minimum of forecast updates are 8 which are
+    # for the minimum days number - 1 day.
     num_values = 8 * forecast_days
     filtered_data = filtered_data[:num_values]
-    # Once the data is filtered based on the 'forecast_days' the user entered, I filter it
-    # again based on the 'kind' the user entered.
-    if kind == "Temperature":
-        # Extracting a list with all the temperatures.
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if kind == "Sky":
-        # Extracting a list with all the sky's kinds.
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
     return filtered_data
 
 
 # The 'get_data()' func will run only when the backend.py file is executed and
 # not when the func is imported to somewhere else.
 if __name__ == "__main__":
-    print(get_data(place="Tokyo", forecast_days=3, kind="Temperature"))
+    print(get_data(place="Tokyo", forecast_days=3))
--- main.py
+++ main.py
@@ -13,9 +13,33 @@ option = st.selectbox("Select data to view",
                       ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {forecast_days} days in {place}")
 
-data = get_data(place, forecast_days, option)
 
-# Creating a 'plotly' figure for the line-graph in the graph.
-#figure = px.line(x= , y= , labels={"x":" ", "y":""})
-# Creating the graph.
-#st.plotly_chart(figure)
+# Getting the data from the backend only in case that the user entered a 'place', so there won't be
+# a "key_error" in the beginning before the user entered a 'place'.
+if place:
+    try:
+        # Getting a list with all the forecast hour updates based on the 'forecast_days' the user entered.
+        filtered_data = get_data(place, forecast_days)
+
+        # Once the data is filtered based on the 'forecast_days' the user entered and I got it in
+        # the 'filtered_data' variable, I filter it again based on the 'option' the user entered.
+        if option == "Temperature":
+            # Extracting a list with all the temperatures, and dividing them by 10 in order to get the accurate temp.
+            temperature = [dict["main"]["temp"] / 10 for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            # Creating a 'plotly' figure for the line-graph in the graph.
+            figure = px.line(x=dates, y=temperature, labels={"x": "Date", "y": "Temperature (C)"})
+            # Creating the Temperature graph.
+            st.plotly_chart(figure)
+
+        if option == "Sky":
+            # Extracting a list with all the sky's cases.
+            sky_cases = [dict["weather"][0]["main"] for dict in filtered_data]
+            # Creating a dictionary that contains all the sky's cases and their corresponding image.
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            # Translating all the sky's cases to their corresponding image.
+            images_path = [images[case] for case in sky_cases]
+            st.image(images_path, width=115)
+    except KeyError:
+        st.info("The place you entered doesn't exist. Please enter an existing place.")
